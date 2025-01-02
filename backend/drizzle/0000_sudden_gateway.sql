601cdb6509dcc8a857da1216ebf20750513e0b12
esutarosa <esutarosa.git@gmail.com>
2025-01-02 22:42:42 +0200

    update(db): change types for database table

--- backend/drizzle/0000_sudden_gateway.sql
+++ backend/drizzle/0000_sudden_gateway.sql
@@ -1,13 +1,13 @@
 CREATE TABLE "users" (
-	"id" text PRIMARY KEY DEFAULT uuid(4) NOT NULL,
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"email" text NOT NULL,
 	"password" text NOT NULL,
 	"username" text NOT NULL,
 	"display_name" text NOT NULL,
 	"avatar" text,
 	"bio" text,
-	"created_at" timestamp (3) DEFAULT now() NOT NULL,
-	"updated_at" timestamp (3) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now(),
 	CONSTRAINT "users_email_unique" UNIQUE("email"),
 	CONSTRAINT "users_username_unique" UNIQUE("username")
 );
