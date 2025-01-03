765f8a9a47e56562c800b5384e1992b0b6bb0e5a
esutarosa <esutarosa.git@gmail.com>
2025-01-03 19:20:37 +0200

    fix(drizzle): update migration path

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
+  out: './migration',
+  schema: './src/core/drizzle/schemas/drizzle.schemas.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
